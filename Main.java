b2593af4e79dc37aadb8d6ec7555dd8246970758
Pavel Snigerev <llianellpp.pp@gmail.com>
2024-02-03 16:11:07 +0300

    Generator

--- Main.java
+++ Main.java
@@ -1,2 +1,12 @@
+import java.util.Random;
+
 public class Main {
+    /*
+        Generator
+    */
+    private static int generateNumber() {
+        Random hiddenNumber = new Random();
+
+        return hiddenNumber.nextInt(1, 10);
+    }
 }
